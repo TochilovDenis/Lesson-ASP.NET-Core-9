7c97f218a8190128cb1524f27c8c86c88acea72e
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-07 18:57:59 +0300

    Для отправки файлов применяется метод SendFileAsync(), который получает либо путь к файлу в виде строки, либо информацию о файле в виде объекта IFileInfo. Например, допустим нам надо отправить файл по адресу "C:\\путь"

---
+++ lesson_04_07.07/SendingFiles/Program.cs
@@ -0,0 +1,6 @@
+﻿var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async (context) => await context.Response.SendFileAsync("C:\\Users\\dex\\Pictures\\Saved Pictures\\i.jpg"));
+
+app.Run();
