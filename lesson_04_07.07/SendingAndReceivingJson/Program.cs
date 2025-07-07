325a6738bcaf15ebfae155c5e3e331e61cc2aed7
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-07 20:22:36 +0300

    Получение JSON. Метод ReadFromJsonAsync https://metanit.com/sharp/aspnet6/2.10.php

--- lesson_04_07.07/SendingAndReceivingJson/Program.cs
+++ lesson_04_07.07/SendingAndReceivingJson/Program.cs
@@ -3,12 +3,34 @@ var app = builder.Build();
 
 app.Run(async (context) =>
 {
-    Person tom = new("Tom", 22);
-    await context.Response.WriteAsJsonAsync(tom);
+    //Person tom = new("Tom", 22);
+    //await context.Response.WriteAsJsonAsync(tom);
 
     //var response = context.Response;
     //response.Headers.ContentType = "application/json; charset=utf-8";
     //await response.WriteAsync("{\"name\":\"Tom\",\"age\":37}");
+
+    var response = context.Response;
+    var request = context.Request;
+    if (request.Path == "/api/user")
+    {
+        var message = "Некорректные данные";   // содержание сообщения по умолчанию
+        try
+        {
+            // пытаемся получить данные json
+            var person = await request.ReadFromJsonAsync<Person>();
+            if (person != null) // если данные сконвертированы в Person
+                message = $"Name: {person.Name}  Age: {person.Age}";
+        }
+        catch { }
+        // отправляем пользователю данные
+        await response.WriteAsJsonAsync(new { text = message });
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
 });
 
 app.Run();
