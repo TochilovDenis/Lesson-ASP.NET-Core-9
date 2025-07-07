562bc384f8ac53b5969bfeb61555ebbb07a0d845
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-07 20:03:47 +0300

    https://metanit.com/sharp/aspnet6/2.6.php Загрузка файла

--- lesson_04_07.07/SendingFiles/Program.cs
+++ lesson_04_07.07/SendingFiles/Program.cs
@@ -7,17 +7,26 @@ app.Run(async (context) =>
     var fullPath = $"html/{path}";
     var response = context.Response;
 
-    response.ContentType = "text/html; charset=utf-8";
+    
     if(path == "/img")
     {
+        response.ContentType = "image/jpg; charset=utf-8";
         await response.SendFileAsync("i.jpg");
     }
     else if (File.Exists(fullPath))
     {
+        response.ContentType = "text/html; charset=utf-8";
         await response.SendFileAsync(fullPath);
+    }
+    else if (path == "/dl")
+    {
+        //save jpg
+        response.Headers.ContentDisposition = "attachment; filename=my_i.jpg";
+        await response.SendFileAsync("i.jpg");
     }
     else
     {
+        response.ContentType = "text/html; charset=utf-8";
         response.StatusCode = 404;
         await response.WriteAsync("<h2>Not Found</h2>");
     }
