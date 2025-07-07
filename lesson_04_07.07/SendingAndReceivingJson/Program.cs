b81c5ef75a91c105ed9ab912ac3d04bb6e2f3e65
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-07 20:12:17 +0300

    Отправка JSON. Метод WriteAsJsonAsync

---
+++ lesson_04_07.07/SendingAndReceivingJson/Program.cs
@@ -0,0 +1,16 @@
+﻿var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async (context) =>
+{
+    Person tom = new("Tom", 22);
+    await context.Response.WriteAsJsonAsync(tom);
+
+    //var response = context.Response;
+    //response.Headers.ContentType = "application/json; charset=utf-8";
+    //await response.WriteAsync("{\"name\":\"Tom\",\"age\":37}");
+});
+
+app.Run();
+
+public record Person(string Name, int Age);
