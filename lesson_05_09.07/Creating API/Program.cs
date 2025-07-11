4fefce4f9696996d3b080153e0e7eb0c7c1fc6f1
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-11 19:01:01 +0300

    09.07

--- lesson_05_09.07/Creating API/Program.cs
+++ lesson_05_09.07/Creating API/Program.cs
@@ -1,6 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using System.Text.RegularExpressions;
+
+// начальные данные
+List<Person> users = new List<Person>
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (context) =>
+{
+    var response = context.Response;
+    var request = context.Request;
+    var path = request.Path;
+    //string expressionForNumber = "^/api/users/([0-9]+)$";   // если id представляет число
+
+    if (path == "/api/users" && request.Method == "GET")
+    {
+        await response.WriteAsJsonAsync(users);
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
+});
 
 app.Run();
+
+
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
