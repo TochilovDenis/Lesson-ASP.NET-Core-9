81b128f959235c287aaf84d85e7f57e9a1b5589c
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-23 18:55:34 +0300

    https://metanit.com/sharp/aspnet6/4.1.php

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System.Text;
+
+var builder = WebApplication.CreateBuilder();
+
+var services = builder.Services;
+
+var app = builder.Build();
+
+app.Run(async context =>
+{
+    var sb = new StringBuilder();
+    sb.Append("<h1>Все сервисы</h1>");
+    sb.Append("<table>");
+    sb.Append("<tr><th>Тип</th><th>Lifetime</th><th>Реализация</th></tr>");
+    foreach (var svc in services)
+    {
+        sb.Append("<tr>");
+        sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+        sb.Append($"<td>{svc.Lifetime}</td>");
+        sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+        sb.Append("</tr>");
+    }
+    sb.Append("</table>");
+    context.Response.ContentType = "text/html;charset=utf-8";
+    await context.Response.WriteAsync(sb.ToString());
+});
+
+app.Run();
+
+interface ILogger
+{
+    void Log(string message);
+}
+class Logger : ILogger
+{
+    public void Log(string message) => Console.WriteLine(message);
+}
+class Message
+{
+    ILogger logger;
+    public string Text { get; set; } = "";
+    public Message(ILogger logger)
+    {
+        this.logger = logger;
+    }
+    public void Print() => logger.Log(Text);
+}
+
